bd24d94a5588af595dd7d5203de54daffc9a9655
FrancoBulotas <francothiago10@hotmail.com>
2025-02-25 20:55:30 -0300

    vercel.json edited

--- api/index.js
+++ api/index.js
@@ -4,11 +4,11 @@ const config = require('./utils/config')
 const path = require('path')
 
 // se encarga de redirigir los routes al archivo index.html de mi front
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// })
 
-const PORT = config.PORT
+const PORT = config.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
