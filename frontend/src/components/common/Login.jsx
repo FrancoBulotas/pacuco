64278645c03f548a1625b0d68dc0b7a369bb6588
FrancoBulotas <francothiago10@hotmail.com>
2025-08-25 20:39:29 -0300

    show and hide pass button added

--- frontend/src/components/common/Login.jsx
+++ frontend/src/components/common/Login.jsx
@@ -1,16 +1,18 @@
 
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import loginService from '../../services/login'
 import guardapolvosServices from '../../services/guardapolvos'
 import { setUser } from '../../reducers/loginReducer'
 import Alert from 'react-bootstrap/Alert'
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 
 const Login = () => {
     const [username, setUsername] = useState('')   
     const [password, setPassword] = useState('') 
     const [alertStatus, setAlertStatus] = useState({display : 'none', text : ''})
+    const [showPassword, setShowPassword] = useState(false);
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
@@ -45,6 +47,8 @@ const Login = () => {
         }      
     }
 
+    const toggleShowPassword = () => setShowPassword(prev => !prev);
+
     return (
         <div className='auth-main-container'>
             <div className='auth-container'>
@@ -57,16 +61,33 @@ const Login = () => {
                         <div className="mb-3">
                             <label className="form-label">Usuario</label>
                             <input  className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                            value={username}
-                            onChange={({ target }) => setUsername(target.value)}  
+                                value={username}
+                                onChange={({ target }) => setUsername(target.value)}  
                             ></input>
                         </div>
                         <div className="mb-3">
                             <label className="form-label">Contraseña</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"
-                            value={password !== undefined ? password : ''}
-                            onChange={({ target }) => setPassword(target.value)} 
-                            ></input>
+
+                            {/* input group para el ojo */}
+                            <div className="input-group">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    className="form-control"
+                                    id="exampleInputPassword1"
+                                    value={password !== undefined ? password : ''}
+                                    onChange={({ target }) => setPassword(target.value)}
+                                    
+                                />
+                                <button
+                                    type="button"
+                                    onClick={toggleShowPassword}
+                                    className="eye-button"
+                                    aria-label={showPassword ? "Ocultar contraseña" : "Mostrar contraseña"}
+                                >
+                                    {showPassword ?  <IoMdEyeOff /> : <IoMdEye />}
+                                </button>
+                            </div>
+
                         </div>
                         <button type="submit" className="btn btn-dark">Ingresar</button>
                         <a href="/register" className="btn ">Registrarse</a>
